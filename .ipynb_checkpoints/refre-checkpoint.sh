1a9536469410fa25477dc547ed0860e3eca9598c
Carlos Ed Beluzo <cbeluzo@gmail.com>
2020-03-28 12:30:16 -0300

    Refresh

--- .ipynb_checkpoints/refre-checkpoint.sh
+++ .ipynb_checkpoints/refre-checkpoint.sh
@@ -1,4 +1,17 @@
-jupyter nbconvert --to notebook --execute covid_19_total_deaths.ipynb
+cd code
+jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute covid_19_sns_population_infection_rate.ipynb
+jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute covid_19_time_serires_daily_growth_rate.ipynb
+jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute covid_19_time_series_comfirmed_cases.ipynb 
+jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute covid_19_total_deaths.ipynb
+jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute covid_19_time_series_population_infection_rate.ipynb
+jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute covid_19_total_deaths.nbconvert.ipynb
+jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute covid_19_time_series_total_deaths.ipynb
+jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute covid_19_total_deaths_animation.ipynb
+
+rm *.nbconvert.ipynb
+
+cd ..
+
 git add .
 git commit -m "Refresh"
 git pull

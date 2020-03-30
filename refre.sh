9175644acc44c858620351207f43adb9eee416e7
Carlos Ed Beluzo <cbeluzo@gmail.com>
2020-03-30 11:50:32 -0300

    Refresh

--- refre.sh
+++ refre.sh
@@ -1,13 +1,13 @@
 while true
 do
-    #jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_sns_population_infection_rate.ipynb
-    #jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_time_serires_daily_growth_rate.ipynb
-    #jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_time_series_comfirmed_cases.ipynb 
-    #jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_total_deaths.ipynb
-    #jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_time_series_population_infection_rate.ipynb
-    #jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_total_deaths.nbconvert.ipynb
-    #jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_time_series_total_deaths.ipynb
-    #jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_total_deaths_animation.ipynb
+    jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_sns_population_infection_rate.ipynb
+    jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_time_serires_daily_growth_rate.ipynb
+    jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_time_series_comfirmed_cases.ipynb 
+    jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_total_deaths.ipynb
+    jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_time_series_population_infection_rate.ipynb
+    jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_total_deaths.nbconvert.ipynb
+    jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_time_series_total_deaths.ipynb
+    jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute code/covid_19_total_deaths_animation.ipynb
     
     rm code/*.nbconvert.ipynb
     git add .
@@ -16,5 +16,5 @@ do
     git push
     
     echo "Press <CTRL+C> to exit."
-    sleep 3600
+    sleep 36000
 done
